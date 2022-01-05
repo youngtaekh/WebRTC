f1af93eac9d1b9d70473ea09b43b97904e6eaadb
youngtaek <youngtaek@peoplev.co.kr>
2022-01-05 10:07:53 +0900

    Develop RTP.Utils

--- rtp/src/main/java/com/young/rtp/util/PCState.kt
+++ rtp/src/main/java/com/young/rtp/util/PCState.kt
@@ -1,4 +1,54 @@
 package com.young.rtp.util
 
+import java.lang.Exception
+
 class PCState {
+    enum class State {
+        IDLE,
+
+        OFFER_PENDING,
+        CREATE_OFFER,
+        SET_LOCAL_OFFER,
+
+        ANSWER_PENDING,
+        SET_REMOTE_OFFER,
+        CREATE_ANSWER,
+
+        CONNECT_PENDING
+    }
+
+    class WrongPCStateException(message: String): Exception(message)
+
+    private var state = State.IDLE
+
+    fun setPCState(state: State) {
+        this.state = state
+    }
+
+    private fun check(state: State, nextState: State): Boolean {
+        return when (state) {
+            State.IDLE -> nextState == State.OFFER_PENDING ||
+                    nextState == State.ANSWER_PENDING ||
+                    nextState == State.IDLE
+            State.OFFER_PENDING -> nextState == State.CREATE_OFFER
+            State.CREATE_OFFER -> nextState == State.SET_LOCAL_OFFER
+            State.SET_LOCAL_OFFER -> nextState == State.CONNECT_PENDING
+
+            State.ANSWER_PENDING -> nextState == State.SET_REMOTE_OFFER
+            State.SET_REMOTE_OFFER -> nextState == State.CREATE_ANSWER
+            State.CREATE_ANSWER -> nextState == State.CONNECT_PENDING
+            State.CONNECT_PENDING -> nextState == State.IDLE
+        }
+    }
+    /*
+    createPeerConnection
+    createOffer
+    setLocalDescription
+    setRemoteDescription
+
+    createPeerConnection
+    setRemoteDescription
+    createAnswer
+    setLocalDescription
+     */
 }
--- rtp/src/main/java/com/young/rtp/util/RTCProximitySensor.kt
+++ rtp/src/main/java/com/young/rtp/util/RTCProximitySensor.kt
@@ -1,4 +1,108 @@
 package com.young.rtp.util
 
-class RTCProximitySensor {
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import org.webrtc.ThreadUtils
+
+class RTCProximitySensor private constructor(
+    context: Context,
+    sensorStateListener: Runnable
+): SensorEventListener {
+    private val threadChecker: ThreadUtils.ThreadChecker = ThreadUtils.ThreadChecker()
+    private val onSensorStateListener: Runnable = sensorStateListener
+    private val sensorManager: SensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    private var proximitySensor: Sensor? = null
+    private var lastStateReportIsNear = false
+
+    init {
+        RTPLog.i(TAG, "RTCProximitySensor ${RTCUtils.getThreadInfo()}")
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        threadChecker.checkIsOnValidThread()
+        RTCUtils.assertIsTrue(event!!.sensor.type == Sensor.TYPE_PROXIMITY)
+        val distanceInCentimeters = event.values[0]
+        if (distanceInCentimeters < proximitySensor!!.maximumRange) {
+            RTPLog.d(TAG, "Proximity sensor => NEAR state")
+            lastStateReportIsNear = true
+        } else {
+            RTPLog.d(TAG, "Proximity sensor => FAR state")
+            lastStateReportIsNear = false
+        }
+
+        onSensorStateListener.run()
+
+        RTPLog.d(TAG, "onSensorChanged ${RTCUtils.getThreadInfo()}: " +
+                "accuracy=${event.accuracy}, timestamp=${event.timestamp}, " +
+                "distance=${event.values[0]}")
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        threadChecker.checkIsOnValidThread()
+        RTCUtils.assertIsTrue(sensor!!.type == Sensor.TYPE_PROXIMITY)
+        if (accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE) {
+            RTPLog.e(TAG, "The values returned by this sensor cannot be trusted")
+        }
+    }
+
+    fun start(): Boolean {
+        threadChecker.checkIsOnValidThread()
+        RTPLog.d(TAG, "start ${RTCUtils.getThreadInfo()}")
+        if (!initDefaultSensor()) {
+            return false
+        }
+        sensorManager.registerListener(this, proximitySensor, SensorManager.SENSOR_DELAY_NORMAL)
+        return true
+    }
+
+    fun stop() {
+        threadChecker.checkIsOnValidThread()
+        RTPLog.d(TAG, "stop ${RTCUtils.getThreadInfo()}")
+        if (proximitySensor == null) return
+        sensorManager.unregisterListener(this, proximitySensor)
+    }
+
+    fun sensorReportsNearState(): Boolean {
+        threadChecker.checkIsOnValidThread()
+        return lastStateReportIsNear
+    }
+
+    private fun initDefaultSensor(): Boolean {
+        if (proximitySensor != null) {
+            return true
+        }
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+        if (proximitySensor == null) {
+            return false
+        }
+        logProximitySensorInfo()
+        return true
+    }
+
+    private fun logProximitySensorInfo() {
+        if (proximitySensor == null) return
+
+        RTPLog.d(TAG, "Proximity sensor: name=${proximitySensor!!.name}, " +
+                "vendor: ${proximitySensor!!.vendor}, " +
+                "power: ${proximitySensor!!.power}, " +
+                "resolution: ${proximitySensor!!.resolution}, " +
+                "max range: ${proximitySensor!!.maximumRange}, " +
+                "min delay: ${proximitySensor!!.minDelay}, " +
+                "type: ${proximitySensor!!.stringType}, " +
+                "max delay: ${proximitySensor!!.maxDelay}, " +
+                "reporting mode: ${proximitySensor!!.reportingMode}, " +
+                "isWakeUpSensor: ${proximitySensor!!.isWakeUpSensor}")
+    }
+
+    companion object {
+        private const val TAG = "RTCProximitySensor"
+
+        @JvmStatic
+        fun create(context: Context, sensorStateListener: Runnable): RTCProximitySensor {
+            return RTCProximitySensor(context, sensorStateListener)
+        }
+    }
 }
