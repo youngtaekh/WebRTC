2c0103a0f2a8daa42a93d7accb0519fcad64589f
youngtaek <youngtaek@peoplev.co.kr>
2022-01-05 10:46:38 +0900

    Develop rtp PeerConnection

--- rtp/src/main/java/com/young/rtp/pc/PCParameters.kt
+++ rtp/src/main/java/com/young/rtp/pc/PCParameters.kt
@@ -1,4 +1,8 @@
 package com.young.rtp.pc
 
-class PCParameters {
-}
+class PCParameters(
+    val isAudio: Boolean,
+    val isVideo: Boolean,
+    val isScreen: Boolean,
+    val isDataChannel: Boolean
+)
